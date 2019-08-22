f2440a190d7d4967426718554c9e4d3185378861
aryunaabhay <aryuna.abhay@gmail.com>
2019-08-21 23:47:17 -0500

    unit tests fixes

--- MovieDBTests/ContentListViewModelTests.swift
+++ MovieDBTests/ContentListViewModelTests.swift
@@ -26,59 +26,103 @@ class ContentListViewModelTests: XCTestCase {
         App.isNetworkReachable = true
     }
     
-    func testMoviesListDataOnline(){
+    func testListDataOnlineSuccess(){
         let contentListVm = ContentListViewModel(contentType: .movie, sortCategory: .popular)
         let path = "/" + App.apiVersion + "/movie/" + contentListVm.sortCategory.rawValue
         stub(condition: isHost(self.domain) && isPath(path)) { _ in
             let stubPath = OHPathForFile("movies.json", type(of: self))
             return fixture(filePath: stubPath!, headers: ["Content-Type":"application/json"])
         }
-        let moviesDataExpectation = expectation(description: "movies list online")
-        contentListVm.retrieveData({
-            moviesDataExpectation.fulfill()
-        })
-        self.waitForExpectations(timeout: 1) { error in
-            XCTAssert(contentListVm.allObjects.count > 0)
-        }
+        self.successListValidations(listViewModel: contentListVm)
     }
     
-    func testListDataLocally(){
+    func testListDataLocallySucess(){
         self.loadDataLocally()
         let contentListVm = ContentListViewModel(contentType: .movie, sortCategory: .popular)
-        let moviesDataExpectation = expectation(description: "movies list offline")
-        contentListVm.retrieveData({
-            moviesDataExpectation.fulfill()
-        })
-        self.waitForExpectations(timeout: 1) { error in
-            XCTAssert(contentListVm.allObjects.count > 0)
-        }
+        self.successListValidations(listViewModel: contentListVm)
     }
     
-    func testSearchOnline(){
+    func testSearchOnlineSuccess(){
         stub(condition: isHost(self.domain) && isPath("/" + App.apiVersion + "/search/movie")) { _ in
             let stubPath = OHPathForFile("movies.json", type(of: self))
             return fixture(filePath: stubPath!, headers: ["Content-Type": "application/json"])
         }
+        self.successSearchValidations()
+    }
+    
+    func testSearchOfflineSuccess(){
+        self.loadDataLocally()
+        self.successSearchValidations()
+    }
+    
+    func testListNodata(){
+        App.isNetworkReachable = false
         let contentListVm = ContentListViewModel(contentType: .movie, sortCategory: .popular)
-        let moviesDataExpectation = expectation(description: "movies search results online")
-        
-        contentListVm.searchContent(by: "Avengers", completion: {
+        let moviesDataExpectation = expectation(description: "movies list")
+        contentListVm.retrieveData({
             moviesDataExpectation.fulfill()
         })
         self.waitForExpectations(timeout: 1) { error in
-            XCTAssert(contentListVm.displayObjects.count > 0)
+            XCTAssertEqual(contentListVm.allObjects.count, 0)
+            XCTAssertEqual(contentListVm.displayObjects.count, 0)
+            XCTAssertEqual(contentListVm.dataState.value, ContentListDataState.noData)
         }
     }
     
-    func testSearchOffline(){
-        self.loadDataLocally()
+    func testSearchNoData(){
+        App.isNetworkReachable = false
         let contentListVm = ContentListViewModel(contentType: .movie, sortCategory: .popular)
-        let moviesDataExpectation = expectation(description: "movies search results offline")
+        let moviesDataExpectation = expectation(description: "movies list")
+        contentListVm.retrieveData({
+            moviesDataExpectation.fulfill()
+        })
+        self.waitForExpectations(timeout: 1) { error in
+            XCTAssertEqual(contentListVm.allObjects.count, 0)
+            XCTAssertEqual(contentListVm.displayObjects.count, 0)
+            XCTAssertEqual(contentListVm.dataState.value, ContentListDataState.noData)
+        }
+    }
+    
+    func testListError(){
+        let contentListVm = ContentListViewModel(contentType: .movie, sortCategory: .popular)
+        let path = "/" + App.apiVersion + "/movie/" + contentListVm.sortCategory.rawValue
+        stub(condition: isHost(self.domain) && isPath(path)) { _ in
+            let notConnectedError = NSError(domain: NSURLErrorDomain, code: URLError.cannotFindHost.rawValue)
+            return OHHTTPStubsResponse(error: notConnectedError)
+        }
+        let moviesDataExpectation = expectation(description: "movies list error")
+        contentListVm.retrieveData({
+            moviesDataExpectation.fulfill()
+        })
+        
+        self.waitForExpectations(timeout: 5) { error in
+            XCTAssertEqual(contentListVm.allObjects.count, 0)
+            XCTAssertEqual(contentListVm.displayObjects.count, 0)
+            XCTAssertEqual(contentListVm.dataState.value, ContentListDataState.errorLoading)
+        }
+    }
+    
+    func successListValidations(listViewModel: ContentListViewModel){
+        let moviesDataExpectation = expectation(description: "movies list")
+        listViewModel.retrieveData({
+            moviesDataExpectation.fulfill()
+        })
+        self.waitForExpectations(timeout: 1) { error in
+            XCTAssert(listViewModel.allObjects.count > 0)
+            XCTAssert(listViewModel.displayObjects.count > 0)
+            XCTAssertEqual(listViewModel.dataState.value, ContentListDataState.loaded)
+        }
+    }
+    
+    func successSearchValidations(){
+        let contentListVm = ContentListViewModel(contentType: .movie, sortCategory: .popular)
+        let moviesDataExpectation = expectation(description: "movies search results")
         contentListVm.searchContent(by: "Toy S", completion: {
             moviesDataExpectation.fulfill()
         })
         self.waitForExpectations(timeout: 1) { error in
             XCTAssert(contentListVm.displayObjects.count > 0)
+            XCTAssertEqual(contentListVm.dataState.value, ContentListDataState.loaded)
         }
     }
     
